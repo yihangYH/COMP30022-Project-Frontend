8f2b7ce7406f987007903cbe5752533f4be34283
Yihang Liu <yihang1@student.unimelb.edu.au>
2022-08-23 18:33:55 +0800

    add home test

--- app/src/component/home.js
+++ app/src/component/home.js
@@ -14,10 +14,12 @@ function Home(){
             <Header btnText="Log in"/>
             <div className='content'>
                 <p>
-                    asdasdasdasdasdasdaddddddddddddd
+                    Everyone goes to many restaurants in their lifetime. Are there any restaurants that have impressed you? Is there any food that keeps you coming back for more?
                 </p>
+                <br></br>
                 <p>
-                    asdasdasdasdasdasdadddddddddddddasdasdasdasdasdasdadddddddddddddasdasdasdasdasdasdadddddddddddddasdasdasdasdasdasdaddddddddddddd
+                    <strong>Restaurant @UniMelb</strong> is an online platform where everyone can share their thoughts on restaurants! You can post your review of any 
+                    restaurant and share your comments with others! You can also upload pictures along with your post and become an ambassador for your favourite restaurant.
                 </p>
             </div>
         </div>
