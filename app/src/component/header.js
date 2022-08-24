235e895fc14c829b2673810eac33626935483b36
Yihang Liu <yihang1@student.unimelb.edu.au>
2022-08-24 19:41:21 +0800

    add logo and router for create post page

--- app/src/component/header.js
+++ app/src/component/header.js
@@ -3,7 +3,9 @@ import '../css/header.css';
 function Header(props){
     return(
         <div className='header'>
-            <div>logo</div>
+            <div>
+                <img className='logo' src={process.env.PUBLIC_URL + '/logo.png'} />
+            </div>
             <div>
                 <button id = "btn" className='login'>{props.btnText}</button>
             </div>
