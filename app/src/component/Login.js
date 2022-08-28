3f221dbfb3a8144b0ed7a51ea2988afb2ba68602
Yihang Liu <yihang1@student.unimelb.edu.au>
2022-08-28 19:31:28 +0800

    restructure login page

--- app/src/component/Login.js
+++ app/src/component/Login.js
@@ -17,36 +17,44 @@ export default function Login() {
     }
   
     return (
-      <div className="Login">
-        <Header display='none'></Header>
-        <div className='Background'>
-            <img src={process.env.PUBLIC_URL + '/background.png'} />
+        <div>
+            <Header display='none' />
+            <div className='login_content'>
+                <ul className='login_content_ul'>
+                    <li>
+                    <div className='Welcome'>
+                        <p>Welcome Back</p>
+                    </div>
+                    <Form onSubmit={handleSubmit}>
+                        <Form.Group size="lg" controlId="email">
+                        <Form.Label>Email</Form.Label>
+                        <Form.Control
+                            autoFocus
+                            type="email"
+                            value={email}
+                            onChange={(e) => setEmail(e.target.value)}
+                        />
+                        </Form.Group>
+                        <Form.Group size="lg" controlId="password">
+                        <Form.Label>Password</Form.Label>
+                        <Form.Control
+                            type="password"
+                            value={password}
+                            onChange={(e) => setPassword(e.target.value)}
+                        />
+                        </Form.Group>
+                        <Button block="true" size="lg" type="submit" disabled={!validateForm()}>
+                        Login
+                        </Button>
+                    </Form>
+                    </li>
+                    <li>
+                        <img  className="login_img" src={process.env.PUBLIC_URL + '/background.png'} />
+                    </li>
+
+                </ul>
+            </div>
+
         </div>
-        <div className='Welcome'>
-            <p>Welcome Back</p>
-        </div>
-        <Form onSubmit={handleSubmit}>
-          <Form.Group size="lg" controlId="email">
-            <Form.Label>Email</Form.Label>
-            <Form.Control
-              autoFocus
-              type="email"
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-            />
-          </Form.Group>
-          <Form.Group size="lg" controlId="password">
-            <Form.Label>Password</Form.Label>
-            <Form.Control
-              type="password"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-            />
-          </Form.Group>
-          <Button block="true" size="lg" type="submit" disabled={!validateForm()}>
-            Login
-          </Button>
-        </Form>
-      </div>
     );
 }
